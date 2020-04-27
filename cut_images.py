cc6c314f215c278761bd392e51dee7b5d41154d0
erelin6613 <valentineburn@gmail.com>
2020-04-27 10:38:53 +0300

    Mask generation step added

--- cut_images.py
+++ cut_images.py
@@ -2,15 +2,8 @@ import os
 import numpy as np
 import rasterio as rio
 import rasterio.plot as rioplot
-import rasterio.mask as mask
-import rioxarray as riox
-import shapely
-import pyproj
-import fiona
+import rasterio.mask as riomask
 import geopandas as gpd
-import matplotlib.pyplot as plt
-import imageio
-import shapefile
 from PIL import Image
 
 dst_crs = 'EPSG:32636'
@@ -20,17 +13,25 @@ def overlap_mask(shapes, profile, tiff_map,
 	target_shape=(CROP_SIZE, CROP_SIZE)):
 
 	transform, crs = tiff_map.profile['transform'], tiff_map.profile['crs']
-	mask_arr = mask.raster_geometry_mask(tiff_map, 
-		shapes)
-	print(mask_arr)
-	return
+	mask_arr = riomask.raster_geometry_mask(tiff_map, 
+		shapes, invert=True)
+	mask_arr = mask_arr[0]*255
+	i=0
+	j=0
+	while i < mask_arr.shape[0]:
+		#print(i, j)
+		while j < mask_arr.shape[1]:
+			img_arr = mask_arr[i:i+CROP_SIZE, j:j+CROP_SIZE]
+			#print(i, j)
+			img_arr = np.array(img_arr, dtype='uint8')
+			img_arr = Image.fromarray(img_arr)
+			img_arr.save(os.path.join('mask', f'{i}_{j}.png'))
+			j += CROP_SIZE
+		i += CROP_SIZE
+		j=0
+		#print(i, mask_arr.shape[0])
 
-	mask = rio.features.rasterize(shapes, 
-		(tiff_map.shape[0], tiff_map.shape[1]), 
-		fill=0,
-		#default_value=255,
-		transform=transform)
-	return mask
+	#return mask
 
 def save_divided_imgs(image, CROP_SIZE=CROP_SIZE):
 	"""
@@ -38,23 +39,25 @@ def save_divided_imgs(image, CROP_SIZE=CROP_SIZE):
 	(channels, height, width)
 	"""
 	i=0
+	j=0
 	img_arr = []
 	image = image.read()
+	# TODO: actual grid
 	while i < image.shape[1]:
-		img_arr = []
-		for channel in range(image.shape[0]):
-			img_arr.append(image[channel, i:i+CROP_SIZE, i:i+CROP_SIZE])
-			#print(img_arr[channel].shape)
-			print(np.array(img_arr[channel]))
-			img = Image.fromarray(np.array(img_arr[channel]))
-			img.save(os.path.join('tci', f'{i}_{channel}.png'))
-		print(np.array(img_arr).shape)
-		img_arr = np.array(img_arr)
-		img_arr = Image.fromarray(np.ma.transpose(img_arr, [1, 2, 0]))
-		img_arr.save(os.path.join('tci', f'{i}_{channel}.png'))
-				#j += CROP_SIZE	
-			#break
+		while j < image.shape[2]:
+			img_arr = []
+			for channel in range(image.shape[0]):
+				img_arr.append(image[channel, i:i+CROP_SIZE, j:j+CROP_SIZE])
+				print(np.array(img_arr[channel]))
+				img = Image.fromarray(np.array(img_arr[channel]))
+				img.save(os.path.join('true', f'{i}_{j}_{channel}.png'))
+			print(np.array(img_arr).shape)
+			img_arr = np.array(img_arr)
+			img_arr = Image.fromarray(np.ma.transpose(img_arr, [1, 2, 0]))
+			img_arr.save(os.path.join('true', f'{i}_{j}_rgb.png'))
+			j += CROP_SIZE
 		i += CROP_SIZE
+		j=0
 
 def get_geoms(shape_paths, crs=dst_crs):
 
@@ -63,14 +66,15 @@ def get_geoms(shape_paths, crs=dst_crs):
 		p = gpd.read_file(file).to_crs(crs)
 		for shape in p.geometry:
 			geoms.append(shape)
-	print(geoms)
+	return geoms
 
 
 if __name__ == '__main__':
 
-	full_map = rio.open('T36UYV_TCI_10m.tif')
+	full_map = rio.open('T36UYV_TCI_10m.jp2', driver='JP2OpenJPEG')
 	shape_paths = [os.path.join('regions', os.listdir('regions')[i]) 
 		for i in range(len(os.listdir('regions')))]
 	geoms = get_geoms(shape_paths, crs=full_map.profile['crs'])
 	print(geoms)
+	overlap_mask(geoms, full_map.profile, full_map)
 	save_divided_imgs(full_map)
